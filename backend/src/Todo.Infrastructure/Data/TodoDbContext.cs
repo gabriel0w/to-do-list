65c2c7c15e786104aadf8a127898ca7a5c2ca0b7
gabriel0w <gabrielo.dzn@gmail.com>
2025-10-16 09:59:31 -0300

    feat(infrastructure): add EF Core DbContext and task mappings (Npgsql)

---
+++ backend/src/Todo.Infrastructure/Data/TodoDbContext.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
+
+namespace Todo.Infrastructure.Data;
+
+public class TodoDbContext : DbContext
+{
+    public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        var task = modelBuilder.Entity<TaskItem>();
+        task.ToTable("tasks");
+        task.HasKey(t => t.Id);
+        task.Property(t => t.Id).ValueGeneratedOnAdd();
+        task.Property(t => t.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+        task.Property(t => t.IsDone)
+            .HasDefaultValue(false);
+        task.Property(t => t.OrderIndex)
+            .HasDefaultValue(0);
+        task.Property(t => t.CreatedAt)
+            .HasDefaultValueSql("NOW() AT TIME ZONE 'UTC'");
+        task.HasIndex(t => new { t.IsDone, t.OrderIndex, t.CreatedAt });
+    }
+}
+
