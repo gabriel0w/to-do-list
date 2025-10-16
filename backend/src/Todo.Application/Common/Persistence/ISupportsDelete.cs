cb7ddeddac65818473564e5c408a74fab21000af
gabriel0w <gabrielo.dzn@gmail.com>
2025-10-16 16:43:02 -0300

    refactor(structure): delete old structure

--- backend/src/Todo.Application/Common/Persistence/ISupportsDelete.cs
+++
@@ -1,9 +0,0 @@
-namespace Todo.Application.Common.Persistence;
-
-public interface ISupportsDelete<TEntity, TId>
-{
-    void Delete(TEntity entity);
-    void Delete(IEnumerable<TEntity> entities);
-    void DeleteById(TId id);
-}
-
--- backend/src/Todo.Application/Tasks/CreateTaskRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace Todo.Application.Tasks;
-
-public class CreateTaskRequest
-{
-    public string Title { get; set; } = string.Empty;
-}
-
